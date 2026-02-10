6bbbfcda63a947fd9377581cb601ecc69a828fcc
punith <punithsramgowda26@gmail.com>
2026-02-10 08:43:27 +0530

    refractoring

--- src/main/java/com/farso/logic/challenge/dsa/strings/lengthOfLongestSubString.java
+++ src/main/java/com/farso/logic/challenge/dsa/strings/lengthOfLongestSubString.java
@@ -7,17 +7,17 @@ public class lengthOfLongestSubString {
     public static void main(String[] args) {
 // Longest Substring Without Repeating Characters --count
 //       sliding window with a HashMap
-        int example1 = findOfLongestSubStringWithoutDuplicated("pppppp");//p --1
+        int example1 = findTheLongestSubStringWithoutDuplicated("pppppp");//p --1
         System.out.println(example1);
-        int example2 = findOfLongestSubStringWithoutDuplicated("pkpkpk");//pk --2
+        int example2 = findTheLongestSubStringWithoutDuplicated("pkpkpk");//pk --2
         System.out.println(example2);
-        int example3 = findOfLongestSubStringWithoutDuplicated("ayappa");//ayp --3
+        int example3 = findTheLongestSubStringWithoutDuplicated("ayappa");//ayp --3
         System.out.println(example3);
 
 
     }
 
-    public static int findOfLongestSubStringWithoutDuplicated(String s) {
+    public static int findTheLongestSubStringWithoutDuplicated(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         int left = 0;
         int maxlength = 0;
